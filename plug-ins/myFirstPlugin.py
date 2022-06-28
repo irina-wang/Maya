1558cc6c9e672e334441d18dfbaa4485b83be75c
Irina Wang <irina.wang@outlook.com>
2022-06-27 23:26:25 -0400

    load first plug-in

---
+++ plug-ins/myFirstPlugin.py
@@ -0,0 +1,42 @@
+import sys
+import maya.OpenMaya as OpenMaya
+import maya.OpenMayaMPx as OpenMayaMPx
+
+# command
+class HelloWorldCmd(OpenMayaMPx.MPxCommand):
+	kPluginCmdName = "spHelloWorld"
+
+	def __init__(self):
+		OpenMayaMPx.MPxCommand.__init__(self)
+
+	@staticmethod
+	def cmdCreator():
+		return OpenMayaMPx.asMPxPtr( HelloWorldCmd() )
+
+	def doIt(self,argList):
+		print('Hello World!')
+
+
+# Initialize the script plug-in
+def initializePlugin(plugin):
+	pluginFn = OpenMayaMPx.MFnPlugin(plugin)
+	try:
+		pluginFn.registerCommand(
+			HelloWorldCmd.kPluginCmdName, HelloWorldCmd.cmdCreator
+		)
+	except:
+		sys.stderr.write(
+			"Failed to register command: %s\n" % HelloWorldCmd.kPluginCmdName
+		)
+		raise
+
+# Uninitialize the script plug-in
+def uninitializePlugin(plugin):
+	pluginFn = OpenMayaMPx.MFnPlugin(plugin)
+	try:
+		pluginFn.deregisterCommand(HelloWorldCmd.kPluginCmdName)
+	except:
+		sys.stderr.write(
+			"Failed to unregister command: %s\n" % HelloWorldCmd.kPluginCmdName
+		)
+		raise
