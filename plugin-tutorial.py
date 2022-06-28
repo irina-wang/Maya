58f1237aff964c6fd72bb7be61ddedfcba8d209a
Irina Wang <irina.wang@outlook.com>
2022-06-27 23:24:39 -0400

    add plugin-tutorial

---
+++ plugin-tutorial.py
@@ -0,0 +1,25 @@
+''' 
+Learning document
+https://help.autodesk.com/view/MAYAUL/2017/ENU/?guid=__files_GUID_FE0662C0_4F1A_46BE_9828_76656C2BD2E2_html
+
+Mengqi Irina Wang 
+
+'''
+
+
+
+''' Maya 1.0 '''
+import maya.api.OpenMaya       as OpenMaya        # Common classes
+import maya.api.OpenMayaAnim   as OpenMayaAnim    # Animation classes
+import maya.api.OpenMayaRender as OpenMayaRender  # Rendering classes
+import maya.api.OpenMayaUI     as OpenMayaUI      # User interface classes
+
+# Maya 1.0
+import maya.OpenMaya       as OpenMaya        # Common classes
+import maya.OpenMayaMPx    as OpenMayaMPx     # Classes from which to inherit
+import maya.OpenMayaAnim   as OpenMayaAnim    # Animation classes
+import maya.OpenMayaFX     as OpenMayaFX      # Effect classes (hair, particles, fluids)
+import maya.OpenMayaRender as OpenMayaRender  # Rendering classes
+import maya.OpenMayaUI     as OpenMayaUI      # User interface classes
+import maya.OpenMayaCloth  as OpenMayaCloth   # Cloth classes
+
