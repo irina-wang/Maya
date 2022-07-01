dff71318fb85e522632b2241b6c98b7c2b365c87
Irina <irina.wang@outlook.com>
2022-07-01 03:19:12 -0400

    add comments and format file by black

--- devkitbase/plug-ins/plug-ins/spZoomCamera.py
+++ devkitbase/plug-ins/plug-ins/spZoomCamera.py
@@ -1,17 +1,31 @@
 from __future__ import division
 
+"""
+reference: https://help.autodesk.com/view/MAYAUL/2022/ENU/?guid=Maya_SDK_py_ref_python_2api1_2py1_zoom_camera_cmd_8py_example_html
+Usage - Zoom in the camera by twice the distance
+Prereq - Create an object in the scene, add camera pointing to the object. 
+
+"""
+
 import sys
 import maya.OpenMaya as OpenMaya
 import maya.OpenMayaUI as OpenMayaUI
 import maya.OpenMayaMPx as OpenMayaMPx
 
-kPluginCmdName = "spZoomCamera"
+"""
+import maya.cmds as cmds
+cmds.loadPlugin("zoomCameraCmd.py")
+cmds.spZoomCamera() 
 
+"""
+
+kPluginCmdName = "spZoomCamera"
 print("zoomCameraCmd.py has been imported....")
 
 # command
 class scriptedCommand(OpenMayaMPx.MPxCommand):
     camera = OpenMaya.MDagPath()
+
     def __init__(self):
         OpenMayaMPx.MPxCommand.__init__(self)
 
@@ -29,33 +43,35 @@ class scriptedCommand(OpenMayaMPx.MPxCommand):
 
     def doIt(self, args):
         global camera
-        camera = OpenMaya.MDagPath()    
+        camera = OpenMaya.MDagPath()
         try:
             OpenMayaUI.M3dView.active3dView().getCamera(camera)
-        except: 
-            sys.stderr.write( "ERROR: getting camera \n" )
+        except:
+            sys.stderr.write("ERROR: getting camera \n")
         else:
             self.redoIt()
 
     def isUndoable(self):
         return True
 
-# Cmd Creator
+    # Cmd Creator
     def cmdCreator():
-        return OpenMayaMPx.asMPxPtr(scriptedCommand() )
-       
-# Initialize the script plug-in 
+        return OpenMayaMPx.asMPxPtr(scriptedCommand())
+
+
+# Initialize the script plug-in
 def initializePlugin(obj):
     plugin = OpenMayaMPx.MFnPlugin(obj)
     try:
         plugin.registerCommand(kPluginCmdName, scriptedCommand.cmdCreator)
     except:
-        sys.stderr.write("Failed to register command: %s\n" % kPluginCmdName )
+        sys.stderr.write("Failed to register command: %s\n" % kPluginCmdName)
 
-# Uninitialize the script plug-in   
+
+# Uninitialize the script plug-in
 def uninitializePlugin(obj):
     plugin = OpenMayaMPx.MFnPlugin(obj)
     try:
-        plugin.deregisterCommand(kPluginCmdName)        
+        plugin.deregisterCommand(kPluginCmdName)
     except:
-        sys.stderr.write("Failed to unregister command: %s\n" % kPluginCmdName )
+        sys.stderr.write("Failed to unregister command: %s\n" % kPluginCmdName)
