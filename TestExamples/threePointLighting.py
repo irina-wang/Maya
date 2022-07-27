dbdd8b101d92b59165bd778bf17bf20fb4c15ad2
Irina <irina.wang@outlook.com>
2022-07-27 13:27:08 -0400

    create light for different objects

--- TestExamples/threePointLighting.py
+++ TestExamples/threePointLighting.py
@@ -16,17 +16,17 @@ def createLightRig(mainObj):
     lightRotation = 30
 
     # create key light
-    newLight = cmds.spotLight(rgb=(1, 1, 1), name="KeyLight")
+    newLight = cmds.spotLight(rgb=(1, 1, 1), name=mainObj + "KeyLight")
     lightTransform = cmds.listRelatives(newLight, parent=True)
     keyLight = lightTransform[0]
 
     # create fill light
-    newLight = cmds.spotLight(rgb=(0.8, 0.8, 0.8), name="FillLight")
+    newLight = cmds.spotLight(rgb=(0.8, 0.8, 0.8), name=mainObj +"FillLight")
     lightTransform = cmds.listRelatives(newLight, parent=True)
     fillLight = lightTransform[0]
 
     # create rim light
-    newLight = cmds.directionalLight(rgb=(0.2, 0.2, 0.2), name="RimLight")
+    newLight = cmds.directionalLight(rgb=(0.2, 0.2, 0.2), name=mainObj +"RimLight")
     lightTransform = cmds.listRelatives(newLight, parent=True)
     rimLight = lightTransform[0]
 
@@ -42,7 +42,7 @@ def createLightRig(mainObj):
     cmds.move(pvtX, pvtY, pvtZ, rimLight + ".rotatePivot")
     cmds.rotate(180 + lightRotation, 0, 0, rimLight)
 
-    lightNode = cmds.group(empty=True, name="LightRig")
+    lightNode = cmds.group(empty=True, name= mainObj + "LightRig")
 
     cmds.parent(keyLight, lightNode)
     cmds.parent(fillLight, lightNode)
@@ -50,42 +50,27 @@ def createLightRig(mainObj):
 
     cmds.select(lightNode, replace=True)
 
+    return keyLight
+
 
 def centerPivot(mainObj):
     cmds.xform(mainObj, cpc=True)
 
+# lightShape - name of the light
+# intensity - float
+def setColor(lightShape, intensity):
+    cmds.setAttr(lightShape + ".intensity", intensity)
+
 
 selectedNodes = cmds.selectedNodes()
 selectedObj = selectedNodes[-1]
-createLightRig(selectedObj)
+lightNode = createLightRig(selectedObj)
+print(lightNode)
+
+# cmds.setAttr(keyLight + ".intensity", 0.5)
+# cmds.setAttr(keyLightShape + ".intensity", 0.5)
 
 
-
-
-sel = cmds.ls(sl=True)[0]
-
-cmds.xform(sel, cpc=True)  # Center its pivot. Comment this out if you don't want to force it to center and use the pivot as-is.
-pivots = cmds.xform(sel, ws=True, q=True, piv=True)[:-3]  # Get its pivot values.
-
-print(pivots)
-
-
-# 
-sel = cmds.ls(sl=True)[0]  # Get selection.
-
-# cmds.xform(sel, cpc=True)  # Center its pivot. Comment this out if you don't want to force it to center and use the pivot as-is.
-# pivots = cmds.xform(sel, q=True, piv=True)[:3]  # Get its pivot values.
-
-# temp_nul = cmds.createNode("transform")  # Create a temporary transform.
-# cmds.matchTransform(temp_nul, sel)  # Align the transform to our object.
-
-# try:
-#     cmds.xform(sel, piv=[0, 0, 0])  # Zero-out object's pivot values.
-#     cmds.move(-pivots[0], -pivots[1], -pivots[2], "{}.vtx[*]".format(sel), os=True, r=True)  # Negate and move object via its old pivot values.
-#     cmds.matchTransform(sel, temp_nul)  # Align the object back to the temporary transform, to maintain its old position.
-# finally:
-#     cmds.delete(temp_nul)  # Delete temporary transform.
-#     cmds.select(sel)  # Restore old selection.
 
 
 
